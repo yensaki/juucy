c6f4707a14be9818e5b850e4be74cf5b6cea3748
yensaki <mov.an.double@gmail.com>
2019-04-07 12:57:21 +0900

    Upload a movie on page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,9 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find_by!(uuid: params[:uuid])
   end
+
+  def create
+    movie = Movie.create!(params.require(:movie).permit(:file))
+    redirect_to movie_url(movie.uuid)
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,12 @@
 class Movie < ApplicationRecord
   has_one_attached :file
   has_many_attached :images
+
+  before_save :generate_uuid
+
+  private
+
+  def generate_uuid
+    self.uuid = SecureRandom.uuid
+  end
 end
