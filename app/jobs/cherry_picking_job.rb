534cf737e8c0d925075e6844e3616307fd969104
yensaki <mov.an.double@gmail.com>
2019-04-07 14:52:57 +0900

    Support S3 adopter

--- app/jobs/cherry_picking_job.rb
+++ app/jobs/cherry_picking_job.rb
@@ -1,11 +1,13 @@
+require 'open-uri'
+
 class CherryPickingJob < ApplicationJob
   queue_as :default
 
   def perform(movie)
-    path =  ActiveStorage::Blob.service.send(:path_for, movie.file.key)
+    file = open(movie.file.blob.service_url)
     dest_dir = Rails.root.join('tmp', movie.uuid)
     FileUtils.mkdir_p(dest_dir)
-    CherryPickingMoments.uniquish!(path, dest_dir)
+    CherryPickingMoments.uniquish!(file.path, dest_dir)
     
     Dir.glob(File.join(dest_dir, '*')).each.with_index(1) do |image_path, index|
       File.open(image_path) do |file|
