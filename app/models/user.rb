d3030feab6185b33b837e7ae2f1fca6520f72a1a
yensaki <mov.an.double@gmail.com>
2020-06-13 23:34:19 +0900

    rm devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  devise :omniauthable, omniauth_providers: %i(twitter)
+  # devise :omniauthable, omniauth_providers: %i(twitter)
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create
