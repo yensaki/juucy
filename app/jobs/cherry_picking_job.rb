4c23ae30bd81b14147928bb9af470083b280a2b3
saki <yensaki@saki-mac.local>
2020-07-02 00:05:59 +0900

    Not use CherryPickingMoments

--- app/jobs/cherry_picking_job.rb
+++ app/jobs/cherry_picking_job.rb
@@ -7,13 +7,13 @@ class CherryPickingJob < ApplicationJob
     file = open(movie.file.blob.service_url)
     dest_dir = Rails.root.join('tmp', movie.uuid)
     FileUtils.mkdir_p(dest_dir)
-    picking_movie = CherryPickingMoments.movie(file.path)
-
-    picking_movie.images.each.with_index(1) do |image, index|
-      File.open(image.filepath) do |file|
-        filename = "#{index}#{File.extname(file)}"
-        movie.images.create!(following_distance: image.following_distance, file: { io: file, filename: filename })
-      end
-    end
+    # picking_movie = CherryPickingMoments.movie(file.path)
+    #
+    # picking_movie.images.each.with_index(1) do |image, index|
+    #   File.open(image.filepath) do |file|
+    #     filename = "#{index}#{File.extname(file)}"
+    #     movie.images.create!(following_distance: image.following_distance, file: { io: file, filename: filename })
+    #   end
+    # end
   end
 end
